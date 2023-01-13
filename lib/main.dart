eed18389f1be932a492fd9057a61cf7a0f0a75e3
Gulab Singh SIkarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2023-01-13 17:31:15 +0530

    Flutter Quiz app  : Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-     
-     
-      appBar: AppBar(
-        title: Text("App title "),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  int questionsIndex = 0;
+  
+  void answerQuestion() {
+    print("index : $questionsIndex");
+
+    questionsIndex = questionsIndex + 1;
+  }
+
+  Widget build(BuildContext context) {
+    var questions = [
+      "Whats your Favorite Color ?",
+      "Whats your Favorite Animal? ",
+      "Whats your Favorite Pet ? ",
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MY APP"),
+        ),
+        body: Column(children: [
+          Text(questions[questionsIndex]),
+          ElevatedButton(onPressed: answerQuestion, child: Text("Answer  1")),
+          ElevatedButton(onPressed: answerQuestion, child: Text("Answer  2")),
+          ElevatedButton(onPressed: answerQuestion, child: Text("Answer  3")),
+        ]),
       ),
-
-
-
-
-      body: Column(
-
-        children: [
-          Text("The question  1 : ) "),
-          ElevatedButton(onPressed: null, child: Text("Answer One ")),
-          ElevatedButton(onPressed: null, child: Text("Answer One ")),
-          ElevatedButton(onPressed: null, child: Text("Answer One ")),
-          ElevatedButton(onPressed: null, child: Text("Answer One ")),
-        ],
-      ),
-    ),
-  ));
+    );
+  }
 }
